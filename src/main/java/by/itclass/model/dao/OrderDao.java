f309dbe17904c279ce7047700d9ac503209d51be
Test User <Test5User@yandex.by>
2024-07-26 19:15:45 +0300

    store 6 drop1

--- src/main/java/by/itclass/model/dao/OrderDao.java
+++ src/main/java/by/itclass/model/dao/OrderDao.java
@@ -1,16 +1,15 @@
 package by.itclass.model.dao;
 
 import by.itclass.model.db.ConnectionManager;
-import by.itclass.model.entities.Order;
-import by.itclass.model.entities.OrderItem;
-import by.itclass.model.entities.Receipt;
-import by.itclass.model.entities.User;
+import by.itclass.model.entities.*;
 import jakarta.servlet.http.HttpSession;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 
 import static by.itclass.constants.DbConst.*;
@@ -20,6 +19,7 @@ public class OrderDao {
     private static final String INSERT_ORDER = "INSERT INTO orders values (?, ?, ?, ?)";
     private static final String INSERT_ORDER_ITEM = "INSERT INTO orderItem values (?, ?, ?, ?, ?)";
     private static final String SELECT_ORDER = "SELECT date, address FROM orders WHERE id = ?";
+    private static final String SELECT_ITEMS = "SELECT itemType, itemId, itemPrice, quantity FROM orderItem WHERE orderId = ?";
 
     private static OrderDao dao;
 
@@ -88,10 +88,47 @@ public class OrderDao {
                 var date = rs.getString(DATE_COL);
                 var address = rs.getString(ADDRESS_COL);
                 receipt.setOrder(new Order(orderId, date, address));
+                var receiptItems = getItemsForReceipt(orderId, cn);
+                receipt.setReceiptItems(receiptItems);
+                receipt.setTotal(Math.round(getTotalAmount(receiptItems)*100)/100d);
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        return receipt;
+    }
 
+    private List<ReceiptItem> getItemsForReceipt(String orderId, Connection cn) throws SQLException {
+        var items = new ArrayList<ReceiptItem>();
+        try (var ps = cn.prepareStatement(SELECT_ITEMS)){
+            ps.setString(1, orderId);
+            var rs = ps.executeQuery();
+            while (rs.next()) {
+                var itemType = rs.getInt(ITEM_TYPE_PARAM);
+                var itemId = rs.getInt(ITEM_ID_PARAM);
+                var itemInfo = getInfo(itemType, itemId, cn);
+                var quantity = rs.getInt(QUANTITY_PARAM);
+                var itemPrice = rs.getDouble(ITEM_PRICE_PARAM);
+                var itemAmount = Math.round(itemPrice * quantity*100)/100d;
+                items.add(new ReceiptItem(itemInfo, itemPrice, quantity, itemAmount));
+            }
+        }
+        return items;
+    }
+
+    private String getInfo(int itemType, int itemId, Connection cn) throws SQLException {
+        try (var st = cn.createStatement()){
+            var tableName = itemType == 1 ? "tv" : "laptop";
+            var query = String.format("SELECT vendor, model FROM %s WHERE id = %d", tableName, itemId);
+            var rs = st.executeQuery(query);
+            rs.next();
+            return String.join("-", rs.getString(VENDOR_COL), rs.getString(MODEL_COL));
+        }
+    }
+
+    private double getTotalAmount(List<ReceiptItem> items) {
+        return items.stream()
+                .map(ReceiptItem::getItemAmount)
+                .reduce(0d, Double::sum);
     }
 }
